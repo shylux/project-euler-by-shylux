90b78f2e36f389721545b2a368e6220ac0e0f1e0
Shylux <shylux@gmail.com>
2012-09-11 11:15:54 +0200

    solved problem 7 and 8

---
+++ 8/main.cc
@@ -0,0 +1,19 @@
+// problem 8
+
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	for (int a = 1; a < 1000; a++) {
+		for (int b = 1; b < 1000; b++) {
+			int c = 1000 - a - b;
+			if (pow(a, 2) + pow(b, 2) == pow(c, 2)) {
+				printf("a: %d, b: %d, c: %d\nproduct: %d\n", a, b, c, a*b*c);
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
